baf75cfa46cc04fbca1ac410ea21c15ae65fd849
1079192443 <yueheng.feng@keylert.com>
2017-03-12 22:39:48 +0800

    Update first.java

--- first.java
+++ first.java
@@ -1,7 +1,3 @@
 public class fistClass{
-<<<<<<< HEAD
-1111112222private String a;
-=======
-111111333333333333private String a;
->>>>>>> 7fb71215480aeea5f294f8dc92723716c8c03d10
+  private String a;
 }
